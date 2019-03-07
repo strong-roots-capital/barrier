71fcfdba55f5c86e636a705220020c41806ce1e9
Eric Crosson <eric.s.crosson@utexas.edu>
2019-03-07 08:48:49 -0600

    Switching gears to subclassing Promise

--- src/barrier.ts
+++ src/barrier.ts
@@ -3,10 +3,57 @@
  * A TypeScript implementation of the synchronization primitive
  */
 
+import is from '@sindresorhus/is'
+
+
+interface PromiseBarrier<T> {
+    pass: (value: T | Error) => T
+
+    count: number
+    // passes: T
+    resolve: (value?: T | PromiseLike<T> | undefined) => void
+    reject: (reason?: any) => void
+}
+
 
 /**
  * TODO: document
  */
-export default function barrier() {
-    // TODO: implement
+export default class Barrier<T> {
+
+    count: number
+    // passes: T
+
+    resolve: (value?: T | PromiseLike<T> | undefined) => void = () => {}
+    reject: (reason?: any) => void = () => {}
+
+    // TODO: separate constructors for count === 1 and count > 1
+    constructor(count = 1) {
+       this.count = count
+
+        const promise: any = new Promise<T>((resolve, reject) => {
+            this.resolve = resolve
+            this.reject = reject
+        })
+        promise.pass = this.pass.bind(this)
+        return promise as Promise<T> & PromiseBarrier<T>
+    }
+
+
+    pass(value?: T | Error) {
+        if (is.error(value)) {
+            return this.reject(value)
+        }
+
+        // this.passes.push(value)
+
+        if (--this.count) {
+            return
+        }
+
+        console.log('resolving value', value)
+        return this.resolve(value)
+    }
+
+
 }
--- test/test-barrier.ts
+++ test/test-barrier.ts
@@ -4,10 +4,55 @@ import test from 'ava'
  * Library under test
  */
 
-import barrier from '../src/barrier'
+import Barrier from '../src/barrier'
 
-test('test ava configuration', t => {
+test('one-count barrier waits for one invocation of pass', async t => {
+    const barrier = new Barrier()
+
+    setTimeout(() => {
+        console.log('passing barrier')
+        barrier.pass()
+    }, 100)
+
+    console.log('awaiting barrier')
+    await barrier
     t.pass()
 })
 
-// TODO: write tests
+test('n-count barrier waits for n invocations of pass', async t => {
+    const n = 2
+    const barrier = new Barrier(n)
+
+    setTimeout(() => {
+        console.log('passing barrier')
+        barrier.pass()
+    }, 100)
+
+    setTimeout(() => {
+        console.log('passing barrier')
+        barrier.pass()
+    }, 150)
+
+    console.log('awaiting barrier')
+    await barrier
+    t.pass()
+})
+
+test.only('one-count barrier should return single passed-value', async t => {
+    const given = 42
+    const barrier = new Barrier<number>()
+
+    setTimeout(() => {
+        console.log('passing barrier')
+        barrier.pass(given)
+    }, 100)
+
+    console.log('awaiting barrier')
+    const value = await barrier
+    t.is(value, given)
+    t.pass()
+})
+
+test.todo('n-count barrier should return array of passed values')
+
+test.todo('barrier should expose .then function of underlying promise')
